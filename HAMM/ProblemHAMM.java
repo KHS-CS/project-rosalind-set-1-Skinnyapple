00638e6c5d573772751a1722f152bab8c29f3cc0
Mr. Alexander Christepher Sun <160359254+Skinnyapple@users.noreply.github.com>
2026-01-22 20:18:24 -0500

    Update ProblemHAMM.java

--- HAMM/ProblemHAMM.java
+++ HAMM/ProblemHAMM.java
@@ -1,22 +1,33 @@
 
 /**
- * @author (your name)
- * @version (a version number or a date)
+ * @Alex
+ * @1.0
  */
 public class ProblemHAMM {
-    public static void main() {
+    public static void main(String[] args) {
         // replace this string with the one from Project Rosalind
         final String inputDNA1 = "GAGCCTACTAACGGGAT";
         final String inputDNA2 = "CATCGTAATGACGGCCT";
         final int output = hammingDistance(inputDNA1, inputDNA2);
-        // you can copy and paste from the terminal window into 
-        //   Project Rosalind to check your answer
+        
+        // print the result so you can copy it to Rosalind
         System.out.println(output);
     }
         
-    // This should take the given strings and return the number
-    //   of times they differ at the same location
+    // Takes two DNA strings of equal length and returns
+    // the number of positions where they differ
     private static int hammingDistance(String a, String b) {
-        return 7;
+        if (a.length() != b.length()) {
+            throw new IllegalArgumentException("Strings must be of equal length");
+        }
+
+        int distance = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                distance++;
+            }
+        }
+
+        return distance;
     }    
 }
